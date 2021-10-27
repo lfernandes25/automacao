7fc0de94303fa3c137a0e20229950aa9bb94dc57
lfernandes25 <leanstorm@hotmail.com>
2021-10-27 18:27:03 -0300

    Adiciona informacoes sobre como executar

--- src/test/java/br/ce/automacao/suporte/BaseUtils.java
+++ src/test/java/br/ce/automacao/suporte/BaseUtils.java
@@ -6,6 +6,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BaseUtils {
 	
 	public static WebDriver createChrome() {
+		/**
+		 * Nescessario extrair o executavel desse caminho src\test\resources\suporte\chromedriver_win32.zip
+		 * e adicionar o caminho do mesmo abaixo, para assim executar a automação
+		 */
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\leans\\Driver\\chromedriver.exe");
 		WebDriver navegador = new ChromeDriver();		
 		navegador.manage().window().maximize();
